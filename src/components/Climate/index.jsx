fe0945819cb91e36da238ac28378a75c9372a6c2
Gustavo Faustino <gfaustino@datagro.intra>
2026-03-06 10:58:08 -0300

    commit add new updates: card news

--- src/components/Climate/index.jsx
+++ src/components/Climate/index.jsx
@@ -1,28 +1,28 @@
 'use client'
- 
+
 import React from 'react'
 import { useTranslation } from 'react-i18next';
- 
+
 export const Climate = () => {
- 
+
     const handleClick = (link) => { window.open(link); }
-    const {i18n} = useTranslation();
-    const langCode = i18n.language === 'pt-BR' ? 'pt_br' : i18n.language === 'en-US' ? 'en' : 'es' ;
- 
+    const { i18n } = useTranslation();
+    const langCode = i18n.language === 'pt-BR' ? 'pt_br' : i18n.language === 'en-US' ? 'en' : 'es';
+
     return (
         <section className="w-full flex justify-center py-8">
             <div className="w-full max-w-[420px] bg-white  shadow-sm ring-1 ring-black/10 overflow-hidden ">
- 
+
                 <div className="px-4 py-3 border-b border-black/10">
                     <p className="text-lg font-semibold text-[#319e96]">
                         Tempo Agora
                     </p>
                 </div>
- 
+
                 <div className="flex justify-center">
                     <iframe
                         id="frameclima"
- 
+
                         src={`https://rotinas.datagro.com/Portal/assets/widgets/clima/${langCode}/cli.php`}
                         scrolling="no"
                         className="border-0 w-[360px] h-[140px] p-8 ml-6"
