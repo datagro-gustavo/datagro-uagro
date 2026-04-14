6190139143aa626ac042d15e5345fc35401ba92f
Gustavo <gustavo.faustino@datagro.com>
2026-04-14 15:26:42 -0300

    commit add new update: notice component

--- src/components/Notice/index.jsx
+++ src/components/Notice/index.jsx
@@ -17,15 +17,36 @@ export const Notice = ({ notice, importanceLevel }) => {
       {animated ?
         <Skeleton width={"90%"} />
         :
-        <div className="relative">
-          <p
-            className={`font-metropolis text-lg font-regular mb-3 ${textColor}`}
-            dangerouslySetInnerHTML={{ __html: notice }}
-          />
-  
-          {/* {importanceLevel == 3 ? <div className=" w-[100%]  absolute bottom-0  bg-[#ffffffec]   h-[50px]" /> : <></>} */}
+        <div
+          className={`
+                    font-metropolis text-lg leading-8 mb-3
+                    ${textColor}
 
-        </div>
+                    [&_h1]:text-3xl [&_h1]:font-bold [&_h1]:mt-10 [&_h1]:mb-5
+                    [&_h2]:text-2xl [&_h2]:font-bold [&_h2]:mt-2 [&_h2]:mb-4
+                    [&_h3]:text-xl [&_h3]:font-semibold [&_h3]:mt-6 [&_h3]:mb-3
+                    
+                    [&_p]:mb-4
+                    [&_a]:underline 
+                    [&_a]:text-[verde do uagro]
+                    [&_a]:text-inherit
+                    [&_a]:hover:text-secondary // isso e do noticias, colocar a cor do uagro
+                    [&_strong]:font-semibold
+                    [&_b]:font-semibold
+                    [&_em]:italic
+                    [&_i]:italic
+
+                    [&_ul]:list-disc [&_ul]:pl-6 [&_ul]:mb-4
+                    [&_ol]:list-decimal [&_ol]:pl-6 [&_ol]:mb-4
+                    [&_li]:mb-2
+
+                    [&_blockquote]:border-l-4 [&_blockquote]:pl-4 [&_blockquote]:italic [&_blockquote]:my-6
+
+                    [&_img]:w-full [&_img]:my-6
+                  `}
+          dangerouslySetInnerHTML={{ __html: notice }}
+        />
+
 
       }
     </div>
