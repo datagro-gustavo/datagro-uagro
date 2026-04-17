6714e4a9daf63cc829a497fffaf19dc011556ef7
Gustavo Faustino <gfaustino@datagro.intra>
2026-04-17 10:33:05 -0300

    commit

--- src/components/Climate/index.jsx
+++ src/components/Climate/index.jsx
@@ -10,17 +10,14 @@ export const Climate = () => {
     const langCode = i18n.language === 'pt-BR' ? 'pt_br' : i18n.language === 'en-US' ? 'en' : 'es';
 
     return (
-        <section className="w-full flex justify-center py-8">
 
+        <div className=''>
+            <iframe
+                src={`https://publicador.uagro.com.br/clima_g/pt_br/cli.php`}
+                className='w-[320px] h-[560px] '
+                scrolling="no"
+            />
+        </div>
 
-                    <iframe
-                        src={`https://publicador.uagro.com.br/clima_g/pt_br/cli.php`}
-                        className='md:h-[280px]'
-
-                    />
-                {/* <div className='w-full border-t border-black/10 hover:bg-black/5 cursor-pointer' onClick={() => handleClick('https://portal.datagro.com/pt/misc/climate/31')}>
-                    <p className="text-md text-center text-black p-2"> Veja Mais</p>
-                </div> */}
-        </section>
     )
 }
